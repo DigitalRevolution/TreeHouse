3d7e034bdd3df54cbdd8d5311c727f2fddb66eec
Digital Revolution <stephen.vandelinder@gmail.com>
2016-02-09 10:15:23 -0700

    included angular built in directives in the application -- updated CSS for better UI. Applied 2-way data binding with change event handling

--- Treehouse_Angular_Basics/scripts/app.js
+++ Treehouse_Angular_Basics/scripts/app.js
@@ -1,8 +1,19 @@
+(function(){
 angular.module("todoListApp", [])
 
   .controller('mainCtrl', function($scope){
     $scope.helloWorld = function(){
     console.log('controller function fired successfully.')
     };
+
+	$scope.todos = [
+	{'name':'Proof Read'},
+	{'name':'GSFS'},
+	{'name':'Send New Articles'},
+	{'name':'Adwords'},
+	{'name':'Visibility Enterprises'},
+	{'name':'Drink Coffee'}
+	];
   }); 
-  
+
+})();
