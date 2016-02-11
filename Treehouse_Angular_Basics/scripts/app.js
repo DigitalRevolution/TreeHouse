1b1751b888a6294afef3b1950714b915cbea0b5a
Digital Revolution <stephen.vandelinder@gmail.com>
2016-02-10 18:58:38 -0700

    Added services to the controller and made UI impromvements.

--- Treehouse_Angular_Basics/scripts/app.js
+++ Treehouse_Angular_Basics/scripts/app.js
@@ -1,19 +1,47 @@
 (function(){
+
 angular.module("todoListApp", [])
 
-  .controller('mainCtrl', function($scope){
-    $scope.helloWorld = function(){
-    console.log('controller function fired successfully.')
+  .controller('mainCtrl', function($scope, dataService){
+
+  	$scope.addTodo = function(){
+  		var todo = {name : "This is a new todo"};
+  		$scope.todos.unshift(todo);  
+  	};
+
+    dataService.getTodos(function(response){
+    	console.log(response.data);
+    	$scope.todos = response.data;
+    });
+
+    $scope.deleteTodo = function(todo, index){
+    	dataService.deleteTodo(todo);
+    	$scope.todos.splice(index, 1);
+    }; 
+
+    $scope.saveTodo = function(todo){
+    	dataService.saveTodo(todo); 
+    };
+  })
+
+  .service('dataService', function($http){
+
+  	this.getTodos = function(callback){
+    	$http.get('mock/todos.json')
+    	.then(callback)
     };
 
-	$scope.todos = [
-	{'name':'Proof Read'},
-	{'name':'GSFS'},
-	{'name':'Send New Articles'},
-	{'name':'Adwords'},
-	{'name':'Visibility Enterprises'},
-	{'name':'Drink Coffee'}
-	];
-  }); 
+    this.deleteTodo = function(todo){
+    	console.log('The ' + todo.name + ' has been deleted.');
+    	// database logic
+    };
+
+    this.saveTodo = function(todo){
+    	console.log('The ' + todo.name + ' was saved.')
+    	// database logic
+    };
+
+  });
+
 
 })();
